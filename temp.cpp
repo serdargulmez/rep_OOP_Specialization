b14d1f81d56141115bca4130ad6a1db669ae19f8
serdargulmez <serdar_gulmez@outlook.com>
2025-03-25 20:54:58 +0300

    cm-04-variable additions were implemented.

--- temp.cpp
+++ temp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,13 +10,16 @@ int main()
     unsigned int spectra_03;
     float spectra_04;
     double spectra_05;
-
+    char spectra_06;
+    string spectra_07;
 
     spectra_01 = true;
     spectra_02 = 123;
     spectra_03 = 59;
     spectra_04 = 45.678;
     spectra_05 = 0.0007;
+    spectra_06 = 'A';
+    spectra_07 = "Hello";
 
     cout << "spectra_01: " << spectra_01 << endl;
     cout << "size of spectra_01: " << sizeof(spectra_01) << endl;
@@ -40,6 +44,18 @@ int main()
     cout << "spectra_05: " << spectra_05 << endl;
     cout << "size of spectra_05: " << sizeof(spectra_05) << endl;
 
+    cout << "-----------------------------------------------------------------" << endl;
 
+    cout << "spectra_06: " << spectra_06 << endl;
+    cout << "size of spectra_06: " << sizeof(spectra_06) << endl;
+
+    cout << "-----------------------------------------------------------------" << endl;
+
+    cout << "spectra_07: " << spectra_07 << endl;
+    cout << "size of spectra_07: " << sizeof(spectra_07) << endl;
+
+    cout << "-----------------------------------------------------------------" << endl;
+
+    
     return 0;
 }
