5f19f05b441b269884de64f25f4d9a6110e8e4dd
Serdar Gülmez <98821393+serdargulmez@users.noreply.github.com>
2025-03-26 20:35:40 +0300

    >>>cm-01-Functions were added to the scripts. (#3)

--- main.cpp
+++ main.cpp
@@ -8,53 +8,93 @@
 #include <iostream>
 using namespace std;
 
+void printMenu()
+{
+    cout << "1: Print Help" << endl;
+    cout << "2: Exchange Stats" << endl;
+    cout << "3: Make an Offer" << endl;
+    cout << "4: Make a Bid" << endl;
+    cout << "5: Print Wallet" << endl;
+    cout << "6: Continue" << endl;
+    cout << "====================================" << endl;
+}
+
+void printHelp()
+{
+    cout << "Your aim is to make money." << endl;
+}
+
+void printMarketStats()
+{
+    cout << "Market looks good." << endl;
+}
+
+void enterOffer()
+{
+    cout << "Make an offer and enter the amount." << endl;
+}
+
+void enterBid()
+{
+    cout << "Make a bid and enter the amount." << endl;
+}
+
+void printWallet()
+{
+    cout << "Your wallet is empty." << endl;
+}
+
+void gotoNextTimeFrame()
+{
+    cout << "Going to next time frame." << endl;
+}
+
+int getUserOption()
+{
+    int userOption;
+    cout << "Type in 1-6" << endl;
+    cin >> userOption;
+    cout << "You chose: " << userOption << endl;
+    return userOption;
+}
+
+void processUserOption(int userOption)
+{
+    if (userOption == 1)
+    {
+        printHelp();
+    }
+    else if (userOption == 2)
+    {
+        printMarketStats();
+    }
+    else if (userOption == 3)
+    {
+        enterOffer();
+    }
+    else if (userOption == 4)
+    {
+        enterBid();
+    }
+    else if (userOption == 5)
+    {
+        printWallet();
+    }
+    else if (userOption == 6)
+    {
+        gotoNextTimeFrame();
+    }
+}
+
 int main()
 {
     cout << "--- Program Started ! ---" << endl;
 
     while (true)
     {
-        cout << "1: Print Help" << endl;
-        cout << "2: Exchange Stats" << endl;
-        cout << "3: Make an Offer" << endl;
-        cout << "4: Make a Bid" << endl;
-        cout << "5: Print Wallet" << endl;
-        cout << "6: Continue" << endl;
-        cout << "====================================" << endl;
-
-        unsigned short userOption;
-
-        cout << "Type in 1-6" << endl;
-        cin >> userOption;
-
-        if (userOption == 0)
-        {
-            cout << "Invalid option. Try to type 1-6 !!!" << endl;
-        }
-        else if (userOption == 1)
-        {
-            cout << "Your aim is to make money." << endl;
-        }
-        else if (userOption == 2)
-        {
-            cout << "Market looks good." << endl;
-        }
-        else if (userOption == 3)
-        {
-            cout << "Make an offer and enter the amount." << endl;
-        }
-        else if (userOption == 4)
-        {
-            cout << "Make a bid and enter the amount." << endl;
-        }
-        else if (userOption == 5)
-        {
-            cout << "Your wallet is empty." << endl;
-        }
-        else if (userOption == 6)
-        {
-            cout << "Going to next time frame." << endl;
-        }
+        printMenu();
+        int userOption = getUserOption();
+        processUserOption(userOption);
     }
 
     return 0;
