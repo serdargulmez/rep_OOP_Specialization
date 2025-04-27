208ea66c56a3b70f8214c23212a0c056a496893c
serdargulmez <serdar_gulmez@outlook.com>
2025-04-27 20:08:39 +0300

    + To do exception handling examples, a new .cpp file was created. + An example of string to double code block was added.

---
+++ topic3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+
+
+int main()
+{
+    double d;
+    std::string s = "asdasd";
+    d = std::stod(s);
+    std::cout << "d: " << d << std::endl;
+
+    return 0;
+}
