cd0d16bfb01034a86f9f15e3614d96105793caa3
serdargulmez <serdar_gulmez@outlook.com>
2025-05-01 21:39:29 +0300

    + Instead of objects creation one by one vector was added.

--- 05_constructer.cpp
+++ 05_constructer.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <vector>
 
 class Square
 {
     public:
         Square( unsigned short _side_1,
                 unsigned short _side_2)
+        : side_1(_side_1),
+          side_2(_side_2)
         {
-            side_1 = _side_1;
-            side_2 = _side_2;
         }
 
         unsigned short side_1;
@@ -17,10 +18,16 @@ class Square
 
 int main()
 {
-    Square squ_01{3,4}, squ_02{2,3};
+    // Square squ_01{3,5}, squ_02{4,6};
+
+    std::vector<Square> squares;
+
+    squares.push_back(Square{3,5});
+    squares.push_back(Square{4,6});
+
     
-    std::cout << "squ_01.side_1: " << squ_01.side_1 << std::endl;
-    std::cout << "squ_02.side_1: " << squ_02.side_1 << std::endl;   
+    std::cout << "squ_01.side_1: " << squares[0].side_1 << std::endl;
+    std::cout << "squ_02.side_1: " << squares[1].side_1 << std::endl;   
 
     return 0;
 }
