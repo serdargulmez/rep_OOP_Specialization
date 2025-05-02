ff145a02c11499ac187fcc9453cbf00812c05882
serdargulmez <serdar_gulmez@outlook.com>
2025-05-02 10:06:50 +0300

    + More iterations way were added.

--- 05_constructer.cpp
+++ 05_constructer.cpp
@@ -29,9 +29,24 @@ int main()
     // std::cout << "squ_01.side_1: " << squares[0].side_1 << std::endl;
     // std::cout << "squ_02.side_1: " << squares[1].side_1 << std::endl;   
 
-    for(Square& sq : squares)
+    for(Square sq : squares) //copy of address
     {
       std::cout << "squares side1: " << sq.side_1 << std::endl;
+    }
+
+    for(Square& sq : squares) // using reference don't need to copy of address
+    {
+      std::cout << "squares side1: " << sq.side_1 << std::endl;
+    }
+
+    for(unsigned int i = 0; i < squares.size(); ++i) // common way
+    {
+      std::cout << "squares side1: " << squares[i].side_1 << std::endl;
+    }
+
+    for(unsigned int i = 0; i < squares.size(); ++i) // object style syntax was used
+    {
+      std::cout << "squares side1: " << squares.at(i).side_1 << std::endl;
     }
 
 
