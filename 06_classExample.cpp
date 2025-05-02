821db8db73caf8359972919b1c84989f09442f18
serdargulmez <serdar_gulmez@outlook.com>
2025-05-02 10:52:39 +0300

    + Vector was added.

--- 06_classExample.cpp
+++ 06_classExample.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
-
-
-class Vec3D{
-
+#include <vector>
+class Vec3D
+{
 public:
     int x;
     int y;
     int z;
 };
 
-Vec3D vec1;
-
-void doSomething()
+int main()
 {
+    Vec3D vec1;
     vec1.x = 2;
     vec1.y = 3;
     vec1.z = 4;
-}
 
+    std::vector<Vec3D> coords;
+    coords.push_back(vec1);
+    coords[0].x = 5;
 
-
-int main()
-{
-    doSomething();
-
+    std::cout << coords[0].x << std::endl;
     return 0;
 }
